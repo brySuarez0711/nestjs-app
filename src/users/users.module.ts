1ede8814fd0d33bcb830dbd0c729264e5ec2827e
Bryan Suárez <bryan@taxo.co>
2024-04-16 16:56:53 -0500

    add a middleware

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,25 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { LoggerMiddleware } from './logger/logger.middleware';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
-export class UsersModule {}
+
+//implement a middleware
+export class UsersModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes({
+      //apply middleware only a get routes
+      path: '/users',
+      method: RequestMethod.GET,
+    });
+  }
+}
