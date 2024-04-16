498dd592f86f9963babb1f1b1f11c1af38055e1f
Bryan Suárez <bryan@taxo.co>
2024-04-16 16:22:25 -0500

    store an user

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,20 @@
+import { IsEmail, IsNotEmpty, IsNumber, IsString, Max } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @Max(100)
+  age: number;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,21 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
-  private users = [
-    {
-      id: 1,
-      name: 'John Doe',
-      phone: '1234567890',
-    },
-    {
-      id: 2,
-      name: 'Jane Doe',
-      phone: '0987654321',
-    },
-  ];
+  private users: any = [];
 
   getUsers() {
     return this.users;
+  }
+
+  createUser(user: CreateUserDto) {
+    this.users.push(user);
+    return {
+      ...user,
+      id: this.users.length + 1,
+    };
   }
 }
